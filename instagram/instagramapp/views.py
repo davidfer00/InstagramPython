8febaf94f17e9915b96cb2b963a28cab77bdc077
Centro Ático <centroatico@202C11E19168961.local>
2017-05-27 12:46:57 -0500

    atico5

--- instagram/instagramapp/views.py
+++ instagram/instagramapp/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from instagramapp.models import *
 
 # Create your views here.
 
@@ -12,8 +14,17 @@ def user(request):
     return render(request, 'user.html')
 
 def create_user (request):
-    print (request.POST[ 'Email' ])
-    print (request.POST[ 'Name' ])
-    print (request.POST[ 'Username' ])
-    print (request.POST[ 'Password' ])
-    return render(request, 'index.html')
+    _email = request.POST[ 'email' ]
+    _username = request.POST[ 'username' ]
+    _name = request.POST[ 'name' ]
+    _password = request.POST[ 'password' ]
+    print (_email)
+    print (_name)
+    print (_username)
+    print (_password)
+    user=User.objects.create_user( username = _username, email = _email, first_name = _name, password=_password)
+    myUser = MiUsuario( usuario = user )
+    myUser.save()
+    print (user)
+    print (user.password)
+    return redirect('login')
